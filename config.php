3acc9ea7f7638f71d0945a1c53c13acd22023d67
Runkey <45352413+runkeychow@users.noreply.github.com>
2018-11-30 14:59:27 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyDI6KC5csr0z7J0vYOwOecbc_Z7iMT9AbM');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'RunToBe');
+define('TITLENAME', 'RunToBe');
+define('EN2DEKEY', '1234567890');
+define('EMAIL', 'q5480274@gmail.com');
+?>
